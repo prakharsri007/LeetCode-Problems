8cfb9c0a60f4fee6b79f9628108159688f587c66
prakharsri007 <1976.prakhar@gmail.com>
2022-11-06 16:39:51 +0530

    9th

---
+++ 35.search insert position.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l=0, h=nums.size()-1;
+        int i,x;
+        if(target<nums[0]) return 0;
+        while(l<=h)
+        {
+            i=(h+l)/2;
+            if(nums[i]==target)
+                return i;
+            else if(nums[i]>target){
+                h=i-1;
+                }
+            else{
+                x=i;
+                l=i+1;
+                }
+        }
+        return x+1;
+    }
+    
+};
