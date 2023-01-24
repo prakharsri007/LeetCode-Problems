ac0d5083d4cc6694e3bc01c069fabdbb1a3ced5d
prakharsri007 <1976.prakhar@gmail.com>
2023-01-24 23:23:21 +0530

    7 testcases left

--- 53. Maximum Subarray.cpp
+++ 53. Maximum Subarray.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
 
-        int sum,fsum=0;
+        int sum,fsum=INT_MIN;
 
         for(int i=0;i<nums.size();i++){
-            int sum=0;
+            sum=0;
             for(int j=i;j<nums.size();j++){
-                if(sum<0) sum=0;
-            sum=sum+nums[j];}
-            fsum=max(fsum,sum);
+            sum=sum+nums[j];
+            fsum=max(fsum,sum);}
         }return fsum;
         
     }
