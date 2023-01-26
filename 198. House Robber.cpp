156f0f078379a8ec87ddf3bc641220551fb541b4
prakharsri007 <1976.prakhar@gmail.com>
2023-01-26 23:01:16 +0530

    dynamic programming

---
+++ 198. House Robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+       int temp[n];
+       temp[0]=nums[0];
+       if(n==1) return nums[0];
+       temp[1]=max(nums[0],nums[1]);
+
+       for(int i=2;i<n;i++){
+           temp[i]=max(temp[i-1],nums[i]+temp[i-2]);
+       } return temp[n-1];
+        
+    }
+};
