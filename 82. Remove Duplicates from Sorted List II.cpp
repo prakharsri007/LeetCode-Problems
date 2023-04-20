2f39ed13ea8571fc34447cbc33746d303609e178
prakharsri007 <1976.prakhar@gmail.com>
2023-04-20 23:55:24 +0530

    linked list - medium question

---
+++ 82. Remove Duplicates from Sorted List II.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==NULL)
+        return NULL;
+        ListNode* new_node=new ListNode();
+        new_node->next=head;
+        ListNode* newhead=new_node;
+
+        ListNode* curr=head;
+        ListNode* p=new_node;
+        while(curr!=NULL){
+            if(curr->next!=NULL && curr->val==curr->next->val){
+                while(curr->next!=NULL && curr->val==curr->next->val){
+                    curr=curr->next;
+                }
+                p->next=curr->next;    
+            }
+            else {
+                p=p->next;
+            }
+            curr=curr->next;
+        }
+        return newhead->next ;
+        
+    }
+};
