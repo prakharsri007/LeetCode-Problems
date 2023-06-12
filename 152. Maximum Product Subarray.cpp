1a142c43a6b208aee64fd6e51568a0fc72afb55f
prakharsri007 <1976.prakhar@gmail.com>
2023-06-12 15:54:53 +0530

    dp/array

---
+++ 152. Maximum Product Subarray.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        if(nums[0]==0 && nums.size()==1){
+            return 0;
+        }
+        int max=1;
+        if(nums[0]!=0){
+        max=nums[0];
+        }
+        else{
+        max=nums[1];
+        }
+        
+        
+        int prod=1;
+        for(int i=0;i<nums.size();i++){
+            
+            prod=prod*nums[i];
+            
+            if(prod>max){
+                max=prod;
+            }
+            if(prod==0){
+                prod=1;
+            }
+        }
+        prod=1;
+        for(int j=nums.size()-1;j>=0;j--){
+            prod=prod*nums[j];
+            
+            if(prod>max){
+                max=prod;
+            }
+            if(prod==0){
+                prod=1;
+            }
+        }
+        return max;
+    }
+};
