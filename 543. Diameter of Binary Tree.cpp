9aa02cfb68b65b86714bfa7717601edc20d8825f
prakharsri007 <1976.prakhar@gmail.com>
2023-06-25 19:27:31 +0530

    Binary Tree

---
+++ 543. Diameter of Binary Tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    //this function is being utilised to make option3
+    int maxDepth(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
+        int left=maxDepth(root->left);
+        int right=maxDepth(root->right);
+
+        int height=max(left,right)+1;
+        return height;
+    }
+    
+    int diameterOfBinaryTree(TreeNode* root) {
+        //base case
+        if(root==NULL){
+            return 0;
+        }
+        //option 1&2 when answer is either only in left subtree or only in right sub tree
+        int option1=diameterOfBinaryTree(root->left);
+        int option2=diameterOfBinaryTree(root->right);
+        //third option when answer is formed by combinig both left and right subtress
+        //we have to calculate separate depths of left and right subtree
+        //so we use the function from maximum depth question **104 wala in leetcode
+        int option3=maxDepth(root->right) + maxDepth(root->left);
+
+        //final answer is the max of all these 3 options
+        int ans=max(option1,max(option2,option3));
+        return ans;
+    }
+};
