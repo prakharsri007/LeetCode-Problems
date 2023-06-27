d708972f33b3eec926e3cd5917948b21c02209f1
prakharsri007 <1976.prakhar@gmail.com>
2023-06-27 17:00:59 +0530

    recursive solution added

--- 70. Climbing Stairs.cpp
+++ 70. Climbing Stairs.cpp
@@ -16,3 +16,21 @@ public:
         
     }
 };
+
+//RECURSIVE SOLUTION
+/*
+class Solution {
+public:
+    //RECURSIVE SOLUTION (refer notebook for explanation)
+    int climbStairs(long long n) {
+        //base case
+        if(n<0) return 0;//no possible ways
+        if(n==0) return 1;//1 way only
+
+        //nth stair pe ya toh n-1 wali stair se aaya hoga ya phir n-2 wali se.
+        long long totalWays=climbStairs(n-1)+climbStairs(n-2);
+        return totalWays;
+        
+    }
+};
+*/
