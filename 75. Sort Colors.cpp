bf0b4d76ee6a6a2ef1caaa8e047fff1a167bdb4e
prakharsri007 <1976.prakhar@gmail.com>
2023-07-05 21:45:45 +0530

    Bubble Sort Solution + Brute Force solution without sorting technique

--- 75. Sort Colors.cpp
+++ 75. Sort Colors.cpp
@@ -1,3 +1,18 @@
+//BUBBLE SORT SOLUTION
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        for(int i=1;i<nums.size();i++){
+            for(int j=0;j<nums.size()-1;j++){
+                if(nums[j]>nums[j+1]){
+                    swap(nums[j],nums[j+1]);
+                }
+            }
+        }        
+    }
+};
+
+/* BRUTE FORCE SOLUTION
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
@@ -31,3 +46,4 @@ public:
         
     }
 };
+*/
