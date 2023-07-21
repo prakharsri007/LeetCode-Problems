9120e6be611699716b2ffda17b14bb888fc3c2ff
prakharsri007 <1976.prakhar@gmail.com>
2023-07-21 22:26:23 +0530

    sort

---
+++ 1051. Height Checker.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> exp;
+        exp=heights;
+        sort(exp.begin(),exp.end());
+        int count=0;
+        for(int i=0;i<heights.size();i++){
+            if(heights[i]!=exp[i]){
+                count++;
+            }
+        }
+        return count;
+        
+    }
+};
