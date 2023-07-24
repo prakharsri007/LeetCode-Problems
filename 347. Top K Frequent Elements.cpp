7218148aceae97c8f7ab5486a2f210fe7bc63fb1
prakharsri007 <1976.prakhar@gmail.com>
2023-07-24 11:25:26 +0530

    mapping

---
+++ 347. Top K Frequent Elements.cpp
@@ -0,0 +1,27 @@
+ class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> top;
+        map <int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        int maxf=-1;
+        int maxe=-1;
+        while(k--){
+            for(auto i:m){
+                if(i.second>maxf){
+                    maxf=i.second;
+                    maxe=i.first;
+                }
+            }
+            top.push_back(maxe);
+            m.erase(maxe);
+            maxe=-1;
+            maxf=-1;
+        }
+        return top;
+        
+        
+    }
+};
