4a24dabda4578459ae5ef77818fd1024693a0631
prakharsri007 <1976.prakhar@gmail.com>
2023-07-27 02:51:07 +0530

    optimised binary tree

--- 110. Balanced Binary Tree.cpp
+++ 110. Balanced Binary Tree.cpp
@@ -33,3 +33,26 @@ public:
         
     }
 };
+
+//OPTIMISED O(n)
+
+class Solution {
+private:
+    int height(TreeNode* root){
+        if(root==NULL) return 0;
+
+        int lh=height(root->left);
+        if(lh==-1) return -1;
+
+        int rh=height(root->right);
+        if(rh==-1) return -1;
+
+        if(abs(lh-rh)>1) return -1;
+
+        return 1+ max(lh,rh);
+    }    
+public:
+    bool isBalanced(TreeNode* root) {
+        return height(root) != -1;
+    }
+};
