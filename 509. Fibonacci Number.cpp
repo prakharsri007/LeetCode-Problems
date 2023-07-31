355bfdbe80cdc346fd9c3732bd17dd29a3af9096
prakharsri007 <1976.prakhar@gmail.com>
2023-07-31 14:26:06 +0530

    dp

--- 509. Fibonacci Number.cpp
+++ 509. Fibonacci Number.cpp
@@ -12,3 +12,22 @@ public:
         
     }
 };
+
+
+//DP Solution
+// DP Solution
+class Solution {
+private:
+    int f(int n,vector<int>& dp){
+        if(n<=1) return n;
+
+        if(dp[n]!=-1) return dp[n];
+
+        return dp[n]=f(n-1,dp)+f(n-2,dp);
+    }    
+public:
+    int fib(int n) {
+        vector<int> dp(n+1,-1);
+        return f(n,dp);
+    }
+};
