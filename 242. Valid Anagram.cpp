ff43998108c0bafa8573dfe9fa9dab0fcd0effd0
prakharsri007 <1976.prakhar@gmail.com>
2023-08-31 18:30:39 +0530

    Maps

--- 242. Valid Anagram.cpp
+++ 242. Valid Anagram.cpp
@@ -14,3 +14,27 @@ public:
         
     }
 };
+//MAP APPROACH
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+        unordered_map<char,int> m1;
+        unordered_map<char,int> m2;
+        for(int i=0;i<s.size();i++){
+            m1[s[i]]++;
+        }
+        for(int i=0;i<s.size();i++){
+            m2[t[i]]++;
+        }
+        
+        for(int i=0;i<s.size();i++){
+            if(m1[s[i]]!=m2[s[i]]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
