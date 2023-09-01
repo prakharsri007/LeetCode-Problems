8a978f2cc0a05f878ca5cac7ddae1ce0363195a8
prakharsri007 <1976.prakhar@gmail.com>
2023-09-02 01:57:50 +0530

    Maps

--- 219. Contains Duplicate II.cpp
+++ 219. Contains Duplicate II.cpp
@@ -3,7 +3,7 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int,int> m;//maps}} integer-->index  (which integer occurs at what index)
         for(int i=0;i<nums.size();i++){
-
+//COUNT FUNCTION in maps checks how many times that key has occured in the map
             if(m.count(nums[i])>0){//yani ki pehle bhi ocuur karr chuka hai then check abs difference
                 if(abs(i-m[nums[i]])<=k){
                     return true;
