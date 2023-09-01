756da8f6beb47b6b9ad31691687d29825cce162b
prakharsri007 <1976.prakhar@gmail.com>
2023-09-02 01:55:36 +0530

    Maps

---
+++ 219. Contains Duplicate II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> m;//maps}} integer-->index  (which integer occurs at what index)
+        for(int i=0;i<nums.size();i++){
+
+            if(m.count(nums[i])>0){//yani ki pehle bhi ocuur karr chuka hai then check abs difference
+                if(abs(i-m[nums[i]])<=k){
+                    return true;
+                }
+            }
+            //agar pehle nahi occur kiya hai tab uski position store karr do
+            m[nums[i]]=i;
+        }
+        //agar pura for loop run hua and abs condition satisfy nahi hui then return false
+        return false;
+        
+    }
+};
