ec63fcd1a0f4427428245d4b9af224b04153f64c
prakharsri007 <1976.prakhar@gmail.com>
2023-10-27 22:03:43 +0530

    easier removing of duplicates recurssion/backtracking

--- 47. Permutations II.cpp
+++ 47. Permutations II.cpp
@@ -1,31 +1,34 @@
 class Solution {
-private:
-    void solve(vector<int> nums,vector<vector<int>>& ans,int index){
+public:
+    void solve(vector<int>nums,vector<vector<int>> &ans,int i){
         //base case
-        if(index >= nums.size()){
+        if(i>=nums.size()){
             ans.push_back(nums);
             return;
         }
-        for(int j=index;j<nums.size();j++){
-            swap(nums[index],nums[j]);
-            solve(nums,ans,index+1);
-            //backtrack
-            swap(nums[index],nums[j]);
+        //for swapping the elements to generate permutations
+        for(int j=i;j<nums.size();j++){
+            swap(nums[i],nums[j]);
+            //recurssion
+            solve(nums,ans,i+1);
+            //backtrack so that we get the original nums back to generate other permutations
+            swap(nums[i],nums[j]);
         }
-    }    
-public:
+    }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> ans;
-        int index=0;
-        solve(nums,ans,index);
-        //after getting all the permutations just remove the duplicates
-
-        sort(ans.begin(), ans.end());
-        auto last = unique(ans.begin(), ans.end());
-        ans.erase(last, ans.end());
-        
-        //after removing return the final answer
-        return ans;
-        
+        int i=0;
+        solve(nums,ans,i);
+        //removing duplicate permutations from the ans vector
+            set<vector<int>> ansset;
+            for(auto it:ans){//vector to set (removes duplicates)
+                ansset.insert(it);
+            }
+            vector<vector<int>> finalans;
+            for(auto s:ansset){//set to vector (because return type is vector)
+                finalans.push_back(s);
+            }
+        //duplicate permutations removed
+        return finalans;
     }
 };
