25a9f8725a424b51dca25e049dde979a0129c7ff
prakharsri007 <1976.prakhar@gmail.com>
2024-02-13 23:42:50 +0530

    array-dp hash table

---
+++ 62. Unique Paths.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dpHashTable(m, vector<int>(n, -1));
+        return recursion(0, 0, m, n,dpHashTable);
+    }
+
+private:
+    int recursion(int i, int j, int m, int n, vector<vector<int>> & dpHashTable) {
+        if (i == m - 1 && j == n - 1) {
+            return 1; // reached the end - count one path
+        }
+        if (i >= m || j >= n) {
+            return 0; // out of bounds bc
+        }
+        if(dpHashTable[i][j]!=-1) {
+            return dpHashTable[i][j];
+        }
+        else {
+            return dpHashTable[i][j] = recursion(i + 1, j, m, n,dpHashTable) + recursion(i, j + 1, m, n,dpHashTable); // Explore both down and right paths recursively
+        }
+    }
+};
